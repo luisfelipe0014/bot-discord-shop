cec3938d8c557b44f7cd3418152895539393871d
luisFelipe <luisfelipe0014@gmail.com>
2021-03-13 19:37:41 -0300

    arrumando o ready

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-module.exports = async(client) => {
+module.exports = async(bot) => {
 
   const avatares = [
     ]
@@ -20,25 +20,25 @@ module.exports = async(client) => {
 
     setInterval(async () => { // controlar o intervalo
       let i = Math.floor(Math.random() * atividades.length + 1) - 1
-        await client.user.setActivity(atividades[i][0], { type: atividades [i][1], url: "https://www.twitch.tv/lender0"});
+        await bot.user.setActivity(atividades[i][0], { type: atividades [i][1], url: "https://www.twitch.tv/lender0"});
     }, 7000); // intervalo
   
     setInterval(async () => {
       let b = Math.floor(Math.random() * status.length + 1) - 1
-        await client.user.setStatus(status[b])
+        await bot.user.setStatus(status[b])
     }, 20000)
   
     /*setInterval(async () => {
       let c = Math.floor(Math.random() * avatares.length + 1) - 1
-        await client.user.setAvatar(avatares[c])
+        await bot.user.setAvatar(avatares[c])
     }, 400000)*/
 
-  client.logger.ready("   BOT CRIADO POR LENDER    ");
+    console.log("   BOT CRIADO POR LENDER    ");
   console.log("-------------------------------");
-  console.log(`[        BOT]: ${client.user.username} Esta Online!`);
+  console.log(`[        BOT]: ${bot.user.username} Esta Online!`);
   console.log(`[    Prefixo]: !`);
-  console.log(`[ Servidores]: ${client.guilds.cache.size}`);
-  console.log(`[     Canais]: ${client.channels.cache.size}`);
-  console.log(`[   Usuarios]: ${client.users.cache.size}`);
+  console.log(`[ Servidores]: ${bot.guilds.cache.size}`);
+  console.log(`[     Canais]: ${bot.channels.cache.size}`);
+  console.log(`[   Usuarios]: ${bot.users.cache.size}`);
   console.log(`[BOOT TIME]: ${process.uptime() * 1000}ms`);
 }
