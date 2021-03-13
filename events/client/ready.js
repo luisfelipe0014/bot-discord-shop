0d662667e4070a756437ff1ad514f2d69f3f87b1
luisFelipe <luisfelipe0014@gmail.com>
2021-03-13 19:32:20 -0300

    ready

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,15 +1,44 @@
-module.exports = async (bot) => {
-  console.log("\nBot " + bot.user.username + " is online!\nDeveloped by Felipe®#1814")
+module.exports = async(client) => {
 
-  const activities_list = [
-    '!help', 
-    `em ${bot.guilds.cache.size.toLocaleString()} servidores!`,
-    '!help', 
-    `em ${bot.guilds.cache.size.toLocaleString()} servidores!`
-  ];
+  const avatares = [
+    ]
+  const atividades = [
+    ["Meu prefixo é ! :D", "WATCHING"],
+    ["Estou sendo desenvolvida pelo Felipe®#0800 esse lindo :3", "STREAMING"],
+    ["Estou na versão Beta qualquer erro ou bug relate no Suporte", "WATCHING"],
+    ["Meu Prefix é !", "LISTENING"],
+    ["Em Desenvolvimento", "WATCHING"],
+    ["Use o Comando !help", "WATCHING"],
+    [`em ${bot.users.cache.size.toLocaleString()} servidores!", "STREAMING`]
+    ]
 
-  setInterval(() => {
-    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-    bot.user.setActivity(activities_list[index], { type: 'WATCHING' });
-  }, 10000)
-};
+     const status = [
+      "online",
+      "dnd",
+      "idle"
+    ];
+
+    setInterval(async () => { // controlar o intervalo
+      let i = Math.floor(Math.random() * atividades.length + 1) - 1
+        await client.user.setActivity(atividades[i][0], { type: atividades [i][1], url: "https://www.twitch.tv/lender0"});
+    }, 7000); // intervalo
+  
+    setInterval(async () => {
+      let b = Math.floor(Math.random() * status.length + 1) - 1
+        await client.user.setStatus(status[b])
+    }, 20000)
+  
+    /*setInterval(async () => {
+      let c = Math.floor(Math.random() * avatares.length + 1) - 1
+        await client.user.setAvatar(avatares[c])
+    }, 400000)*/
+
+  client.logger.ready("   BOT CRIADO POR LENDER    ");
+  console.log("-------------------------------");
+  console.log(`[        BOT]: ${client.user.username} Esta Online!`);
+  console.log(`[    Prefixo]: !`);
+  console.log(`[ Servidores]: ${client.guilds.cache.size}`);
+  console.log(`[     Canais]: ${client.channels.cache.size}`);
+  console.log(`[   Usuarios]: ${client.users.cache.size}`);
+  console.log(`[BOOT TIME]: ${process.uptime() * 1000}ms`);
+}
